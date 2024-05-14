50400d0cb08b25f91906588a6cabac91dc202d17
Peter Aimasiko <peteraimasiko@Peters-MacBook-Pro.local>
2024-05-14 18:49:37 -0400

    added routers

--- new-music-recs/src/app/page.js
+++ new-music-recs/src/app/page.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 import {app, auth} from "../../firebase/clientApp";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { getFirestore, collection } from "firebase/firestore";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   
+  const router = useRouter();
 
   const [test, testLoading, testError] = useCollection(
     collection(getFirestore(app), "test"),
@@ -20,10 +22,10 @@ export default function Home() {
       <div className="flex justify-between w-[90%]">
         <div className="flex">
           <img id="note" src="/white.jpg" className=" w-20 h-auto" alt="hello"/>
-          <p id="header" className="poetsen-one-regular mt-4" >New Music Recs</p>
+          <button onClick={() => router.push("/")} id="header" className="poetsen-one-regular mt-4" >New Music Recs</button>
         </div>
         <div className="w-fit px-5 py-2 h-9 mt-3 rounded-md shadow bg-gray-900 text-center flex justify-center items-center">
-          <button className="w-fit h-7 text-center text-violet-100 text-base font-semibold font-['DM_Sans']">Sign in with Google</button>
+          <button onClick={() => router.push("/login")} className="w-fit h-7 text-center text-violet-100 text-base font-semibold font-['DM_Sans']">Sign in with Google</button>
         </div>
       </div>
       <div className=" w-[80rem] h-full text-center text-violet-100 text-4xl font-[500] leading-[3rem] whitespace-pre font-['DM Sans']">
@@ -36,7 +38,7 @@ export default function Home() {
         <img id="tidal" className="w-20 h-20" src="/white.jpg" alt="tidal"></img>
       </div>
       <div className="w-36 h-12 bg-gray-900 rounded-md shadow-lg text-center flex justify-center items-center">
-        <button className=" text-violet-100 text-xl font-semibold font-['DM Sans']">Get Started</button>
+        <button onClick={() => router.push("/login")}  className=" text-violet-100 text-[1.1rem] font-semibold font-['DM Sans']">Get Started</button>
       </div>
     </div>
   );
